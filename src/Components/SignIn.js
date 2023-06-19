a6f964dcad00f84d9e928b6f569a226f524db2d4
marikomands <marikomands@gmail.com>
2023-06-19 14:24:59 +0100

    changed style

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  // fetchSignInMethodsForEmail,
+} from "firebase/auth";
 import { auth } from "../firebase";
 
 function SignIn() {
+  // const [email, setEmail] = useState("");
+  // const [accounts, setAccounts] = useState([]);
+
+  // const handleEmailChange = (event) => {
+  //   setEmail(event.target.value);
+  // };
+
+  // const handleCheckAccounts = async () => {
+  //   const methods = await fetchSignInMethodsForEmail(auth, email);
+  //   setAccounts(methods);
+  // };
+
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
@@ -11,7 +27,9 @@ function SignIn() {
 
   return (
     <div>
-      <Button onClick={signInWithGoogle}>Loggin In with Google </Button>
+      <Button style={{ fontSize: "20px" }} onClick={signInWithGoogle}>
+        Sign In with Google
+      </Button>
     </div>
   );
 }
